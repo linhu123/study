66be1e505ec8e4ce3f643ad539f1daf99c23277d
linhu123 <1318066698@qq.com>
2019-08-22 18:39:45 +0800

    study p19

--- src/main/java/com/linfix/concurrency/first/chapter06/BankRunable.java
+++ src/main/java/com/linfix/concurrency/first/chapter06/BankRunable.java
@@ -5,10 +5,10 @@ public class BankRunable {
     public static void main(String[] args) {
         final TicketWindowRunable t = new TicketWindowRunable();
         Thread t1 = new Thread(t,"窗口一");
-        t1.start();
         Thread t2 = new Thread(t,"窗口二");
-        t2.start();
         Thread t3 = new Thread(t,"窗口三");
+        t1.start();
+        t2.start();
         t3.start();
 
     }
--- src/main/java/com/linfix/concurrency/first/chapter06/TicketWindowRunable.java
+++ src/main/java/com/linfix/concurrency/first/chapter06/TicketWindowRunable.java
@@ -3,13 +3,21 @@ package com.linfix.concurrency.first.chapter06;
 
 public class TicketWindowRunable implements Runnable{
 
-    private String name;
-    private static final int MAX = 50;
+    private static final int MAX = 500;
     private static int index = 1;
+    private final Object MONITOR = new Object();
     @Override
     public void run() {
-        while (index <= MAX){
-            System.out.println("窗口"+this.name+"票号为："+(index++));
+        synchronized (MONITOR){
+            while (index <= MAX){
+                try {
+                    Thread.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("窗口"+Thread.currentThread().getName()+"票号为："+(index++));
+            }
         }
+
     }
 }
